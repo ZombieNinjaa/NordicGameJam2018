f093623cade68659c0d1d5ec3aacc31f8f0fea11
Doug Joseph <doug.joseph@hotmail.com>
2018-04-14 10:01:01 +0100

    The game is afoot!
    Creation of Unity Project

---
+++ NordicGameJam2018/Assets/Scripts/Raycast.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.up); //transform.position, );
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (hit.collider != null)
+            {
+                Debug.Log("hit " + hit.rigidbody.name);
+            }
+        }
+    }
+}
